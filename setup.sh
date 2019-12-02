86a6790dc486369837aaa614b1dfa9431e8d9049
Ivan Zaykov <contemplator1998@gmail.com>
2019-12-02 04:42:25 +0300

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+sudo apt install gnupg2 pass
+
+# install golang and define PATH
+sudo apt-get install -y --no-install-recommends golang
+mkdir /home/pi/go
+export GOPATH=/home/pi/go
+
+# download the docker-credential-helpers repository
+cd $GOPATH
+go get github.com/docker/docker-credential-helpers
+
+# compile docker-credential-pass and 
+cd $GOPATH/src/github.com/docker/docker-credential-helpers/
+make pass
+# move the compiled sources to /usr/bin
+sudo cp bin/docker-credential-pass /usr/bin
+
+sudo apt-get remove golang
+sudo apt-get autoremove
+
+gpg --generate-key  
+
+read gpgid
+
+pass init $gpgid
